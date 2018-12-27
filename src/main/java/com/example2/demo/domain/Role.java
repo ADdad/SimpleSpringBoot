cae5550182ced199739593241c52a8514df1e036
ADdad <rokden80@gmail.com>
2018-12-27 21:12:04 +0200

    Init commit

---
+++ src/main/java/com/example2/demo/domain/Role.java
@@ -0,0 +1,12 @@
+package com.example2.demo.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum  Role implements GrantedAuthority {
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
