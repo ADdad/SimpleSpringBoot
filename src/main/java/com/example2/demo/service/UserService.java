28e3f7540b803e830b01624abb949804a454bdcf
ADdad <rokden80@gmail.com>
2019-01-04 00:24:41 +0200

    Email sending and confirmation added

--- src/main/java/com/example2/demo/service/UserService.java
+++ src/main/java/com/example2/demo/service/UserService.java
@@ -1,24 +1,69 @@
 package com.example2.demo.service;
 
+import com.example2.demo.domain.Role;
+import com.example2.demo.domain.User;
 import com.example2.demo.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepo userRepo;
 
+    private final MailSenderService mailSenderService;
+
     @Autowired
-    public UserService(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, MailSenderService mailSenderService) {
         this.userRepo = userRepo;
+        this.mailSenderService = mailSenderService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDb != null){
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepo.save(user);
+
+        if(!StringUtils.isEmpty(user.getEmail())){
+            String message = String.format(
+                    "Hello, %s! \n " +
+                            "Welcome to Sweater. Please, visit next link to activate your account: " +
+                            "http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSenderService.send(user.getEmail(), "Activation code", message);
+        }
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if (user == null) return false;
+
+        user.setActivationCode(null);
+
+        userRepo.save(user);
+        return true;
     }
 }
--- src/main/resources/templates/parts/login.ftl
+++ src/main/resources/templates/parts/login.ftl
@@ -2,16 +2,26 @@
     <form action="${path}" method="post">
         <div class="form-group">
             <label for="exampleInputEmail1">User name</label>
-            <input type="text" name="username" class="form-control col-sm-6" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username">
+            <input type="text" name="username" class="form-control col-sm-6" id="exampleInputEmail1"
+                   aria-describedby="emailHelp" placeholder="Enter username">
         </div>
+        <#if isRegisterForm>
+            <div class="form-group">
+                <label for="exampleInputEmail">Email</label>
+                <input type="email" class="form-control col-sm-6" id="exampleInputEmail" name="email"
+                       placeholder="Email">
+            </div>
+        </#if>
         <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control col-sm-6" id="exampleInputPassword1" name="password" placeholder="Password">
+            <input type="password" class="form-control col-sm-6" id="exampleInputPassword1" name="password"
+                   placeholder="Password">
         </div>
+
         <input type="hidden" name="_csrf" value="${_csrf.token}"/>
         <button type="submit" class="btn btn-primary"><#if isRegisterForm>Create <#else> Login</#if></button>
         <#if !isRegisterForm>
-        <a href="/registration">Register</a>
+            <a href="/registration">Register</a>
         </#if>
     </form>
 </#macro>
