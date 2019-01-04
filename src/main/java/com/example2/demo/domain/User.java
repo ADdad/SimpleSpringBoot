607d34fcbc551aad69068bbfa827473fa2666630
ADdad <rokden80@gmail.com>
2019-01-04 23:02:45 +0200

    Password confirm refactor

--- src/main/java/com/example2/demo/domain/User.java
+++ src/main/java/com/example2/demo/domain/User.java
@@ -20,8 +20,6 @@ public class User implements UserDetails {
     @NotBlank(message = "Password cannot be empty")
     private String password;
 
-    @Transient
-    private String password2;
     private boolean active;
 
     @Email(message = "Email is not correct")
@@ -58,13 +56,7 @@ public class User implements UserDetails {
         this.id = id;
     }
 
-    public String getPassword2() {
-        return password2;
-    }
 
-    public void setPassword2(String password2) {
-        this.password2 = password2;
-    }
 
     public String getUsername() {
         return username;
